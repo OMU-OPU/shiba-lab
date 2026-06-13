5a112da9fec6a61cda49a6953cfd44c0d9c96db0
OceanArrow283 <peperoncino1822.univ@gmail.com>
2026-06-13 16:47:58 +0900

    11

---
+++ www.facebook.com/sharer/sharerc8ae.php
@@ -0,0 +1,15 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from www.facebook.com/sharer/sharer.php?u=https://www.omu.ac.jp/eng/shibahara/info/news/entry-44974.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 12 Jun 2026 02:48:40 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=https://www.facebook.com/share_channel/?type=reshare%26link=https%3A%2F%2Fwww.omu.ac.jp%2Feng%2Fshibahara%2Finfo%2Fnews%2Fentry-44974.html%26app_id=966242223397117%26source_surface=external_reshare%26display%26hashtag"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="https://www.facebook.com/share_channel/?type=reshare%26link=https%3A%2F%2Fwww.omu.ac.jp%2Feng%2Fshibahara%2Finfo%2Fnews%2Fentry-44974.html%26app_id=966242223397117%26source_surface=external_reshare%26display%26hashtag"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from www.facebook.com/sharer/sharer.php?u=https://www.omu.ac.jp/eng/shibahara/info/news/entry-44974.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 12 Jun 2026 02:48:40 GMT -->
+</HTML>
