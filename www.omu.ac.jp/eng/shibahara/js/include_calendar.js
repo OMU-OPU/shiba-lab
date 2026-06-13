5fd5b385799969d34525bfda6df508b3b809014b
OceanArrow283 <peperoncino1822.univ@gmail.com>
2026-06-13 16:09:03 +0900

    6

---
+++ www.omu.ac.jp/eng/shibahara/js/include_calendar.js
@@ -0,0 +1,2 @@
+let divCalendar = document.getElementById('js-calendar');
+divCalendar.insertAdjacentHTML('beforeend', '<!--CalendarTime start.--><!--CalendarTime took 0.0121 seconds to process this block.-->');
